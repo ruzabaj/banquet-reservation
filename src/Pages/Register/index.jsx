6a301c53dc1ca7a621f618fce995a6d76e42f1a8
ruzabaj <ruja.bajracharya@gmail.com>
2023-03-16 15:07:06 +0545

    added: router improved: design integrated: filter api

--- src/Pages/Register/index.jsx
+++ src/Pages/Register/index.jsx
@@ -3,11 +3,6 @@ import Heading from './../../Components/Heading/index';
 import CustomerInfo from '../../Components/Forms/customerInfo'
 import "../../Assets/Styles/Form/form.scss";
 import "../../Assets/Styles/Btn.scss";
-import AdvancePayment from '../../Components/Forms/advancePayment';
-import BanquetReservation from '../../Components/Forms/banquetReservation';
-import SpecialRequest from '../../Components/Forms/specialRequest';
-import RateInfo from './../../Components/Forms/rateInfo';
-import SubmitBtn from '../../Components/Buttons/submitBtn';
 
 const Register = () => {
     
--- src/Pages/Started/index.jsx
+++ src/Pages/Started/index.jsx
@@ -4,10 +4,13 @@ import AccordionDetail from './../../Components/Accordion/index';
 import axios from 'axios';
 import "../../Assets/Styles/Filter/filter.scss";
 import DatePickerInput from './../../Components/Datepicker/index';
+import { Link } from 'react-router-dom';
 
 const Started = () => {
   let baseUrl = process.env.REACT_APP_BASE_URL;
   const [detailList, setDetailList] = useState([]);
+  const [customerName, setCustomerName] = useState("");
+  const [state, setState] = useState("");
   const [token, setToken] = useState("");
 
   useEffect(() => {
@@ -16,7 +19,7 @@ const Started = () => {
 
   useEffect(() => {
     axios.post(`${baseUrl}/getStarted`, {
-      "token": `test`
+      token: `test`
     })
       .then((response) => {
         console.log(response.data, "get started api")
@@ -25,50 +28,81 @@ const Started = () => {
       .catch((error) => {
         console.log(error)
       })
+
   }, [])
+
   const [rangeOne, setRangeOne] = useState(new Date());
   const [rangeTwo, setRangeTwo] = useState(new Date());
   let selectedFirstDate = rangeOne.toISOString().substring(0, 10);
   let selectedSecondDate = rangeTwo.toISOString().substring(0, 10);
 
-  // console.log(rangeOne)
-  // console.log(rangeTwo)
+  const handleCustomerName = (event) => {
+    setCustomerName(event.target.value)
+  }
+  const handleBtnChange = (e) => {
+    setState(e.target.value)
+  }
+  const handleFilter = () => {
+    axios.post(`${baseUrl}/banquetreport`, {
+      token: `test`,
+      customerName: `${customerName}`,
+      state: `${state}`,
+      reservationDate: `${selectedFirstDate}`,
+      reservationForDate: `${selectedSecondDate}`,
+    })
+      .then((response) => {
+        console.log(response.data, "filter api")
+        setDetailList(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
   return (
     <div>
       <Navbar />
+      <div className='info-reservation'>
+        <Link to="/">
+          <div className='style-btn-reservation'>
+            <button className='btn-reservation'>Make a reservation</button>
+          </div>
+        </Link>
+      </div>
       <div className='flex-filter-sort'>
         <div className='filter-accordions'>
           <label>Filter:</label>
           <div className='btn-radio'>
-            <input type="radio" id="vehicle1" name="vehicle1" value="Started" />
+            <input type="radio" id="Started" name="state" value="Started" onChange={handleBtnChange} />
             <label > Started</label>
           </div>
           <div className='btn-radio'>
-            <input type="radio" id="vehicle3" name="vehicle3" value="Finalised" />
+            <input type="radio" id="Finalised" name="state" value="Finalised" onChange={handleBtnChange} />
             <label >Finalised</label>
           </div>
           <div className='btn-radio'>
-            <input type="radio" id="vehicle2" name="vehicle2" value="Cancelled" />
+            <input type="radio" id="Cancelled" name="state" value="Cancelled" onChange={handleBtnChange} />
             <label > Cancelled</label>
           </div>
         </div>
         <div className='sort-accordions'>
           <label>Sort By:</label>
           <div className='sorting-input'>
-            <input type="text" placeholder="Name/ Outlets" />
+            <input type="text" placeholder="Name/ Outlets" onChange={handleCustomerName} />
           </div>
         </div>
-        <div className='date-range-picker'>
+        <div>
           <label> Range :</label>
-          <div className='start-date'>
-            <DatePickerInput selectedDate={rangeOne} setSelectedDate={setRangeOne} />
-          </div>
-          <div className='end-date'>
-            <DatePickerInput selectedDate={rangeTwo} setSelectedDate={setRangeTwo} />
+          <div className='date-range-picker'>
+            <div className='start-date'>
+              <DatePickerInput selectedDate={rangeOne} setSelectedDate={setRangeOne} />
+            </div>
+            <div className='end-date'>
+              <DatePickerInput selectedDate={rangeTwo} setSelectedDate={setRangeTwo} />
+            </div>
           </div>
         </div>
       </div>
-      <div className='btn-filter'>
+      <div className='btn-filter' onClick={handleFilter}>
         <button className='filter'>Filter</button>
       </div>
       <AccordionDetail detailList={detailList} />
